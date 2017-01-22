0cdb66eb308e2b7625daeca7032d510cd67ded6d
ImXico <fjm.cunha@campus.fct.unl.pt>
2017-01-22 12:31:16 +0000

    HIERO SAVED US, HE IS A TRUE HIERO

--- core/src/com/theend/game/Game.kt
+++ core/src/com/theend/game/Game.kt
@@ -7,7 +7,7 @@ import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.theend.game.res.ResourceHandler
 import com.theend.game.state.StateManager
-import com.theend.game.state.game.MenuState
+import com.theend.game.state.game.EndGameState
 
 class Game : ApplicationAdapter() {
 
@@ -24,7 +24,7 @@ class Game : ApplicationAdapter() {
         ResourceHandler.init("pack.pack")
         batch = SpriteBatch()
         manager = StateManager()
-        manager.setState(MenuState(manager))
+        manager.setState(EndGameState(true, 100000, 100000, 0, manager))
     }
 
     override fun resize(width: Int, height: Int) {
